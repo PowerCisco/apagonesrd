f81315fdc275558561cc06b29d4e1c144587c8ee
PowerCisco <46621773+PowerCisco@users.noreply.github.com>
2024-02-05 11:51:52 -0400

    Innit commit

---
+++ lib/apagonesrd.ex
@@ -0,0 +1,18 @@
+defmodule Apagonesrd do
+  @moduledoc """
+  Documentation for `Apagonesrd`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Apagonesrd.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/apagonesrd_test.exs
@@ -0,0 +1,8 @@
+defmodule ApagonesrdTest do
+  use ExUnit.Case
+  doctest Apagonesrd
+
+  test "greets the world" do
+    assert Apagonesrd.hello() == :world
+  end
+end
